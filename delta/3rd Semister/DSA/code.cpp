ce30a663496f854dd2e95710a200d1ce484d1648
Atosh Mohonto <135542605+AtoshMohonto@users.noreply.github.com>
2025-06-08 01:16:45 +0600

    adding
    adding on github

---
+++ delta/3rd Semister/DSA/code.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+// cout <<"sajib \n" <<"sagorika"<< endl ;
+int age;
+// age=7;
+cout <<"enter your age:";
+cin >> age; 
+cout << age "your age is:"<< endl;
+return 0;
+}
